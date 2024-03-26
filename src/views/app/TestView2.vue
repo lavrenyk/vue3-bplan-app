869e8bb25d5f46f17237a17d7b3e3a1bee93dc78
ārXāngēl <lavrenyk@gmail.com>
2024-03-27 00:30:35 +1000

    update Test2 page

--- src/views/app/TestView2.vue
+++ src/views/app/TestView2.vue
@@ -12,7 +12,166 @@
             <b>RA-04144</b> | Robinson R-44 | S/N - 1162
           </p>
         </div>
-        <ul class="nav nav-underline mb-3 fs-9" id="myTab" role="tablist">
+        <div class="row g-5 mb-5 mb-lg-8">
+          <div class="col-12 col-xl-4 col-xxl-3">
+            <div class="card border h-100 w-100 overflow-hidden">
+              <div class="bg-holder d-block bg-card"
+                :style="{ backgroundImage: 'url(' + require('@/assets/img/bg/bg-right-26.png')+ ')', backgroundPosition: 'top right'}"></div>
+                <!--/.bg-holder-->
+                <div class="d-dark-none">
+                  <div class="bg-holder d-none d-sm-block d-xl-none d-xxl-block bg-card"
+                    :style="{ backgroundImage: 'url(' + require('@/assets/img/spot-illustrations/32.png')+ ')', backgroundPosition: 'top right'}"></div>
+                  <!--/.bg-holder-->
+                  </div>
+                  <div class="d-light-none">
+                  <div class="bg-holder d-none d-sm-block d-xl-none d-xxl-block bg-card"
+                    :style="{ backgroundImage: 'url(' + require('@/assets/img/spot-illustrations/32.png')+ ')', backgroundPosition: 'top right'}"></div>
+                    <!--/.bg-holder-->
+                  </div>
+                  <div class="card-body px-3 position-relative">
+                    <h4 class="mb-2">Hellicopter Robinson R-44 </h4>
+                    <div class="border-bottom border-translucent mb-3">
+                      <ul class="list-group list-group-flush text-800">
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Aircraft Registration:</span>
+                            </span>
+                            <span>RA-04144</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Aircraft Serial Number:</span>
+                            </span>
+                            <span>1162</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Aircraft Manufacturing Date:</span>
+                            </span>
+                            <span>25.03.2002</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Aircraft Total Hours:</span>
+                            </span>
+                            <span>172,90</span>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="border-bottom border-translucent mb-3">
+                      <h6 class="pb-2 border-bottom border-translucent">Engine</h6>
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Type:</span>
+                            </span>
+                            <span>Lycoming O-540F1B5</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Serial Number:</span>
+                            </span>
+                            <span>L-26573-40A</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Manufacturing Date:</span>
+                            </span>
+                            <span>01.04.2006</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Total Hours:</span>
+                            </span>
+                            <span>172,90</span>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="border-bottom border-translucent">
+                      <h6 class="pb-2 border-bottom border-translucent">Misc</h6>
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">RTR 460 MM:</span>
+                            </span>
+                            <span>Rev. dated AUG 2022</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Lycoming 60297-10 OM:</span>
+                            </span>
+                            <span>Rev. dated MAR 2009</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">MP:</span>
+                            </span>
+                            <span>PS MP R44-N</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">MTOW:</span>
+                            </span>
+                            <span>1089,0</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Empty Weight:</span>
+                            </span>
+                            <span>670,6</span>
+                          </div>
+                        </li>
+                        <li class="list-group-item bg-transparent list-group-crm fs-9 py-1">
+                          <div class="d-flex justify-content-between">
+                            <span class="fw-normal fs-9 mx-1">
+                              <span class="fw-bold">Control Date:</span>
+                            </span>
+                            <span>20.06.2015</span>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 col-lg-8">
+                <div class="col-12 h-100">
+                  <div class="d-flex align-items-center border border-translucent rounded-3 text-center p-5 h-100"
+                    :style="{ backgroundImage: 'url(' + require('@/assets/img/products/R44-demo.jpg')+ ')', backgroundPosition: 'center'}">
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+        <ul class="nav nav-underline mb-3 mt-4 fs-9" id="myTab" role="tablist">
           <li class="nav-item">
             <a class="nav-link active" id="part1-tab"
               data-bs-toggle="tab" href="#tab-part1" role="tab"
@@ -35,6 +194,7 @@
             </a>
           </li>
         </ul>
+
         <div class="tab-content" id="myTabContent">
           <div class="tab-pane fade show active" id="tab-part1" role="tabpanel" aria-labelledby="part1-tab">
             <div class="table-responsive scrollbar">
@@ -874,12 +1034,12 @@
             <h3>PART 3</h3>
             Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork.
           </div>
-            <div class="tab-pane fade show active" id="tab-part4" role="tabpanel" aria-labelledby="part4-tab">
+            <div class="tab-pane fade show" id="tab-part4" role="tabpanel" aria-labelledby="part4-tab">
               <h3>PART 4</h3>
               Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone.
             </div>
         </div>
-        
+
       </div>
     </div>
   </div>
